d31d28db48568409d5687ea1b0c2bf6a76ead7a2
tdbao1 <122350451+tdbao1@users.noreply.github.com>
2023-01-11 11:05:06 +0700

    feat<sign-in>:sign in api (#2)
    add unit test for sign in api

--- src/v1/controllers/auth.controller.ts
+++ src/v1/controllers/auth.controller.ts
@@ -1,9 +1,27 @@
 import {Response, Request, RequestHandler} from 'express';
 import * as authService from '../services/auth.service';
+import {
+    ErrorResponse
+} from '../types/common.type';
+import logger from '../core/loggers';
+import redis from '../databases/init.redis';
+
 
 export const signIn:RequestHandler = async (req: Request, res: Response) => {
-    //do some thing
-    return res.status(200).json('api sign in');
+    const {email, password} = req.body;
+    try {
+        const payload = await authService.signIn({email, password});
+        redis.set(payload.id + '_token', payload.token, (done) => {
+            logger.info('redis set token:::', done);
+        });
+        redis.set(payload.id + '_refreshtoken', payload.refreshToken, (done) => {
+            logger.info('redis set refresh token:::', done);
+        });
+        return res.status(200).json(payload);
+    } catch (error) {
+        const err = error as ErrorResponse;
+        return res.status(err.status).json(err);
+    }
 };
 
 export const signUp: RequestHandler = async (req: Request, res: Response) => {
